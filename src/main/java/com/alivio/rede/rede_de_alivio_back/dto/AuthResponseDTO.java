e8d7a7949becc6d059d7b5f6fbfcf2e9598719d1
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-20 08:29:36 -0300

    add: token

--- src/main/java/com/alivio/rede/rede_de_alivio_back/dto/AuthResponseDTO.java
+++ src/main/java/com/alivio/rede/rede_de_alivio_back/dto/AuthResponseDTO.java
@@ -4,45 +4,23 @@ public class AuthResponseDTO {
     private Integer id;
     private String nome;
     private String email;
+    private String token;
 
     public AuthResponseDTO() {}
 
-    public AuthResponseDTO(Integer id, String nome, String email) {
+    public AuthResponseDTO(Integer id, String nome, String email, String token) {
         this.id = id;
         this.nome = nome;
         this.email = email;
+        this.token = token;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "AuthResponseDTO{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
 }
